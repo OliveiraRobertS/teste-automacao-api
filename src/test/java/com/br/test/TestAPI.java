b1c24489ec116747eda9201c81a66f5949bcf819
Robert <oliveira_roberts@hotmail.com>
2020-04-29 23:25:49 -0300

    Teste API

---
+++ src/test/java/com/br/test/TestAPI.java
@@ -0,0 +1,27 @@
+﻿package com.br.test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Test;
+
+
+public class TestAPI {
+	
+	public TestAPI() {
+		baseURI = "https://reqres.in/api/users?page=2";
+	}
+
+	@Test
+	public void teste() {
+		
+		/*Chama o serviço pelo metodo GET*/
+	            given()
+	           .contentType("application/json")
+	           .when()
+	    	   .get(baseURI)
+	    	   .then()
+	    	   .statusCode(200);	 
+		}
+
+}
